b46607a63f477dbeaf9da454621e25b2c95ed454
purva <purvajg@gmail.com>
2019-03-23 23:38:49 -0700

    Changed package name

--- src/com/server/Server.java
+++ src/com/server/Server.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
--- tst/com/server/ServerTest.java
+++ tst/com/server/ServerTest.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.server;
 
 import org.junit.Test;
 import org.mockito.Mockito;
